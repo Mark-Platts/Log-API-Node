ecb27923431b74692ee4258ffbab4f2c4e6b8417
Mark-Platts <markplattstutoring@outlook.com>
2021-04-19 22:48:13 +0100

    added files

---
+++ sp.js
@@ -0,0 +1,26 @@
+class Log {
+    constructor(logNumber, logDateTime, logEntry) {
+      this.logNumber = logNumber;
+      this.logDateTime = logDateTime;
+      this.logEntry = logEntry;
+    }
+  }
+
+let logs = [];
+
+function addLog(entry) {
+    const logNumber = logs.length;
+    const date = new Date();
+    const logDateTime = date.toUTCString();
+    const newLog = new Log(logNumber, logDateTime, entry);
+    logs.push(newLog);
+}
+
+
+
+addLog('This is a test log');
+
+addLog('This is a second test log');
+
+addLog('This is a third test log');
+
