6f923c75908d1dd6a3e2d2534055017320e8b26b
Mark-Platts <markplattstutoring@outlook.com>
2021-08-03 23:55:01 +0100

    updated style, menu functionality done

--- client/logic.js
+++ client/logic.js
@@ -1,6 +1,10 @@
 const menu = {
+    pageCount: 0,
     pageNumber: 1,
-    logs: [null, null, null, null, null, null]
+    logs: [null, null, null, null, null, null],
+    logLoaded: false,
+    creatingLog: false,
+    editingLog: false
 };
 
 
@@ -11,6 +15,13 @@ async function updateMenu(pageNumber) {
     menu.logs = logJson;
 }
 
+async function updatePageCount() {
+    const endpoint = logPath + 'count';
+    const response = await fetch(endpoint);
+    const countJson = await response.json();
+    menu.pageCount = Math.ceil(countJson.count/6);
+}
+
 function fillMenu() {
     for (let i=0; i<6; i++) {
         fillMenuItems(i+1, menu.logs[i]);
@@ -19,7 +30,7 @@ function fillMenu() {
 
 function fillMenuItems(itemNumber, log) {
     if (log) {
-        hold = `<p>${log.author}</p><p>${log._id}</p><p>${log.subject}</p>`;
+        hold = `<p>${log.author}</p><p>${log.timeStamp}</p><p>${log.subject}</p>`;
         document.getElementById("menuLog"+String(itemNumber)).innerHTML = hold;
     }
     else {
@@ -28,15 +39,56 @@ function fillMenuItems(itemNumber, log) {
     }
 }
 
-//for the > button on the page, increases the page number by 1 and updates the menu ADD VALIDATION TO MAKE SURE PAGE NUMBER IS VALID
-async function pageNumberUp() {
+//for the > button on the page, increases the page number by 1 and updates the menu
+async function pageTurnUp() {
     menu.pageNumber++;
+    document.getElementById("pageNumber").innerHTML = 'Page: ' + String(menu.pageNumber);
     await updateMenu(menu.pageNumber);
     fillMenu();
+}
+
+//for the < button on the page, decreases the page number by 1 and updates the menu
+async function pageTurnDown() {
+    if (menu.pageNumber >= 2) {
+        menu.pageNumber--;
+        document.getElementById("pageNumber").innerHTML = 'Page: ' + String(menu.pageNumber);
+        await updateMenu(menu.pageNumber);
+        fillMenu();
+    }
+}
+
+//for the << button on the page, sets page number = 1 and updates the menu
+async function pageTurnStart() {
+    menu.pageNumber = 1;
+    document.getElementById("pageNumber").innerHTML = 'Page: ' + String(menu.pageNumber);
+    await updateMenu(menu.pageNumber);
+    fillMenu();
+}
+
+//for the >> button on the page, sets page number = pageCount and updates the menu
+async function pageTurnEnd() {
+    menu.pageNumber = menu.pageCount;
+    document.getElementById("pageNumber").innerHTML = 'Page: ' + String(menu.pageNumber);
+    await updateMenu(menu.pageNumber);
+    fillMenu();
+}
+
+
+//onclick for log tabs, loads log info into main screen
+function loadLog(menuNumber) {
+    const log = menu.logs[menuNumber];
+    document.getElementById("logTop").innerHTML = '<p style="display:inline;">Author: '
+                                                    +String(log.author)
+                                                    +'</p><p style="display:inline;">Date: '+String(log.timeStamp)
+                                                    +'</p><p style="display:inline;">Subject: '+String(log.subject)+'</p>';
+    document.getElementById("logDisplay").innerHTML = String(log.content);
+    menu.logLoaded = true;
 }
 
 //function for all on-load logic
 async function initialize() {
     await updateMenu(menu.pageNumber);
+    await updatePageCount();
     fillMenu();
 }
+
